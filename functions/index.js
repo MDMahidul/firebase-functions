257750f3d8965d0762be6182553492e9059a818d
MD. Mahidul Islam <47059408+MDMahidul@users.noreply.github.com>
2021-01-23 22:36:14 +0600

    Add upvoting function
    create a new onCall function for update unvoting and add it to the frontend click event

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const { auth } = require('firebase-admin');
 admin.initializeApp();
 
 // auth trigger (new user signup)
@@ -42,4 +43,39 @@ exports.addRequest = functions.https.onCall((data, content)=>{
             return reject(e);
         });
     });
+});
+
+//upvote callable function
+exports.upvote = functions.https.onCall((data,context)=>{
+    //check auth
+    if(!context.auth){
+        throw new functions.https.HttpsError(
+            'unauthenticated',
+            'only authenticated users can add requests'
+        );
+    }
+    //get refs for user doc and request doc
+    const user = admin.firestore().collection('users').doc(context.auth.uid);
+    const request = admin.firestore().collection('requests').doc(data.id);
+
+    return user.get().then(doc=>{
+        //check user hasn't already upvoted the request
+        if(doc.data().upvotedOn.includes(data.id)){
+            throw new functions.https.HttpsError(
+                'failed-precondition',
+                'You can upvoted something once'
+            );
+        }
+
+        //update user array
+        return user.update({
+            upvotedOn: [...doc.data().upvotedOn,data.id]
+        })
+        .then(()=>{
+            //updates votes on the request
+            return request.update({
+                upvotes: admin.firestore.FieldValue.increment(1)
+            });
+        })
+    })
 });
